492ce78a762f403d33c60a8442866e61ed13b640
Evertz Hamilton <hamiltonevertz99@gmail.com>
2026-02-24 00:17:26 -0400

    feat: project MVP finished

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 // where your node app starts
 
 // init project
+const dns = require('dns').promises;
 require('dotenv').config();
 var express = require('express');
 var app = express();
@@ -9,6 +10,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
 var cors = require('cors');
+const { hostname } = require('os');
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -19,9 +21,29 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+const ipfinder = async (HOST) => {
+  try {
+    const adresses = await dns.resolve4(HOST);
+    return adresses[0];
+  } catch (error) {
+    throw error;
+  }
+};
+
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', async function (req, res) {
+  let language = req.headers['accept-language'];
+  let software = req.headers['user-agent'];
+
+  const host = req.headers['host'].includes(':')
+    ? req.headers['host'].split(':')[0]
+    : req.headers['host'];
+
+  res.json({
+    ipaddress: `${await ipfinder(host)}`,
+    language: `${language}`,
+    software: `${software}`,
+  });
 });
 
 // listen for requests :)
